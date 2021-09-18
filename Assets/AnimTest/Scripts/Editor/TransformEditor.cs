f05f515d89318ebc4a4a0001117e8ae2915deade
Jin Park <jinpark.dev@gmail.com>
2021-09-18 18:55:04 +0900

    Show world pos

--- Assets/AnimTest/Scripts/Editor/TransformEditor.cs
+++ Assets/AnimTest/Scripts/Editor/TransformEditor.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,13 +8,14 @@ public class TransformEditor : Editor
     {
         base.OnInspectorGUI();
 
-        EditorGUILayout.Vector3Field("World Rot",((Transform) target).rotation.eulerAngles);
-        EditorGUILayout.Vector3Field("Local Rot",((Transform) target).localRotation.eulerAngles);
+        EditorGUILayout.Vector3Field("World Pos", ((Transform) target).position);
+        EditorGUILayout.Vector3Field("World Rot", ((Transform) target).rotation.eulerAngles);
+        EditorGUILayout.Vector3Field("Local Rot", ((Transform) target).localRotation.eulerAngles);
         var AB = ((Transform) target).GetComponent<ArticulationBody>();
         if (AB == default) return;
-        if(AB.jointPosition.dofCount == 3)
-            EditorGUILayout.Vector3Field("Anchor Rot",new Vector3(AB.jointPosition[0], AB.jointPosition[1], AB.jointPosition[2]) / Mathf.PI * 180);
-        else if(AB.jointPosition.dofCount == 1)
-            EditorGUILayout.Vector3Field("Anchor Rot",new Vector3(0, 0, AB.jointPosition[0]) / Mathf.PI * 180);
+        if (AB.jointPosition.dofCount == 3)
+            EditorGUILayout.Vector3Field("Anchor Rot", new Vector3(AB.jointPosition[0], AB.jointPosition[1], AB.jointPosition[2]) / Mathf.PI * 180);
+        else if (AB.jointPosition.dofCount == 1)
+            EditorGUILayout.Vector3Field("Anchor Rot", new Vector3(0, 0, AB.jointPosition[0]) / Mathf.PI * 180);
     }
 }
